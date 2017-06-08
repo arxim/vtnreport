08468c390232a2baf3a9893d9484344bd63e6cc1
Eakarat <Eakarat@Eakarat-Scap>
2017-06-08 10:23:58 +0700

    utf-8 edit

--- src/com/scap/vtnreport/controller/getEmailContentSrvl.java
+++ src/com/scap/vtnreport/controller/getEmailContentSrvl.java
@@ -30,6 +30,7 @@ public class getEmailContentSrvl extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 //		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html; charset=UTF-8");
 		doPost(request, response);
 	}
 
@@ -38,6 +39,7 @@ public class getEmailContentSrvl extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		response.setContentType("text/html; charset=UTF-8");
 		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/pages/menu_send_email/send_mail.jsp");
 //		rd.include(request, response);
 		rd.forward(request, response);
