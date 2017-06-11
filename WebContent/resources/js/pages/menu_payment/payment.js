50aca6f5527304b4e30c2819980afa3c716661c0
dubbril <dubbril@gmail.com>
2017-06-12 00:46:31 +0700

    Edit View Report

--- WebContent/resources/js/pages/menu_payment/payment.js
+++ WebContent/resources/js/pages/menu_payment/payment.js
@@ -26,3 +26,15 @@ function getPayment(){
 function getEmail(){
 	$('#frmEmail').submit();
 }
+
+function getReport(){
+	
+	var yyyy = $('#dwlYear').val();
+	var mm = $('#dwlMonth').val();
+	var hospitalCode = $('#hidHospitalCode').val();
+	var	report = $('#dwlReport').val();
+	var doctorCode = $('#txtDoctorCode').val();
+
+	window.open('/vtnreport/DoctorReportSrv?yyyy='+yyyy+'&mm='+mm+'&hospitalCode='+hospitalCode+'&report='+report+'&doctorCode='+doctorCode,'_blank')
+
+}
